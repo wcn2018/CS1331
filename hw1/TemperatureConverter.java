7255f74ed200ab9700cd4027f0073a8b6a172de2
William <wcn2018@gmail.com>
2019-01-15 16:25:55 -0500

    hw1 updated

--- hw1/TemperatureConverter.java
+++ hw1/TemperatureConverter.java
@@ -1,9 +1,12 @@
+//Worked with Bryan Zhao.
+import java.util.Scanner;
+import java.lang.Math;
 public class TemperatureConverter{
 	public static void main(String[] args) {
-		Scanner kelvin = new Scanner(System.in);
+		Scanner Kelvin = new Scanner(System.in);
 		System.out.println("Enter a temperature in Kelvin: ");
-		float k = kelvin.nextLine();
-		float f = (float)k*9/5 - 459.67; 
+		Double k = Kelvin.nextDouble();
+		Double f = k*9/5 - 459.67; 
 		System.out.println(k+" Kelvin is "+f+" degrees Fahrenheit\n");
 	}
 }
