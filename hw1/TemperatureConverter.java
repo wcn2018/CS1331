36e1515a05516ccad4e679ca9e1ef61c39b2dd27
William <wcn2018@gmail.com>
2019-01-15 19:41:09 -0500

    hw1 update probably done

--- hw1/TemperatureConverter.java
+++ hw1/TemperatureConverter.java
@@ -1,12 +1,22 @@
-//Worked with Bryan Zhao.
+//Worked with Bryan Zhao (also in 1331 Statsko B)
+//Used mostly the notes and TA
+//Stack overflow for the string output %6.2f stuff and setting up java.
 import java.util.Scanner;
 import java.lang.Math;
 public class TemperatureConverter{
 	public static void main(String[] args) {
 		Scanner Kelvin = new Scanner(System.in);
 		System.out.println("Enter a temperature in Kelvin: ");
-		Double k = Kelvin.nextDouble();
-		Double f = k*9/5 - 459.67; 
-		System.out.println(k+" Kelvin is "+f+" degrees Fahrenheit\n");
+		String inp = Kelvin.nextLine();
+		double k;
+		if (inp.contains(".")){
+			k = Double.parseDouble(inp);
+		}else{
+			k = Integer.parseInt(inp);
+		}
+		Double fl = ((double)k)*9/5 - 459.67; 
+		System.out.printf(inp + " Kelvin is %6.2f"+" degrees Fahrenheit\n", fl);
+		//how does the %.2f stuff work
+		Kelvin.close();
 	}
 }
